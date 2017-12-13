db6334aa0428c7b62c07351ed00737cd2db215ff
Ian Coughlan <ian@embeddedarm.com>
2017-12-13 10:20:24 -0800

    Added additional commands.

--- ts7800ctl.c
+++ ts7800ctl.c
@@ -36,7 +36,7 @@
          P1.5, 1.8V
    6,7   P2.0, VIN (scaled 5.3%)
    8,9   P2.1  5.2V_A (scaled 44%)
-   10,11 P2.2  AN_3.3V
+   10,11 P2.2  AN_3.3V (scaled 50%)
    12,13 P2.3  ADC_4 (scaled 50%)
    14,17 reserved
    18,19 P2.4  ADC_3 (scaled 50%)
@@ -46,13 +46,20 @@
    
 */ 
 
- 
-volatile unsigned int *data, *control, *status, *led, *syscon;
+#define SILABS_CHIP_ADDRESS 0x54
+
+volatile unsigned int *data, *control, *status, *led, *syscon, *cpuregs;
 static unsigned int verbose, addr;
-static int model, done;
+static int model, done, twifd;
 
 static int get_model(void);
 static int silabs_init(void);
+static int silabs_read(unsigned short, unsigned short, unsigned char *);
+static int parseMacAddress(const char *str, unsigned char *buf);
+static inline void feed_wdt(int);
+static inline void disable_wdt(void);
+static inline void enable_wdt(void);
+static inline void do_silabs_sleep(unsigned int);
 
 static void exit_gracefully(int signum) {
 	fprintf(stderr, "EXITING....\n");
@@ -65,38 +72,70 @@ static void usage(char **);
 static int parsechans(const char*, unsigned int);
 
 
-static int twifd;
+
+static void usage(char **argv)
+{
+   fprintf(stderr, "Usage: %s [OPTION] ...\n"
+	  "Examine/Modify state of TS-7800-V2 hardware.\n"
+	  "\n"
+	  "General options:\n"
+	  "  -s    seconds         Number of seconds to sleep for\n"
+	  "  -f                    Feed the WDT for 8s\n"
+	  "  -d                    Disable the WDT\n"
+	  "  -r    CHANS           Sample ADC channels CHANS, e.g. \"0-2,4\", \"1,3,4\""
+                                 "output raw data to standard out\n"
+     "  -S    CHANS           Sample ADC channels CHANS, e.g. \"0-2,4\", \"1,3,4\""
+                                  "output string parseable data to standard out\n"
+	  "  -A    ADDR            Write DATA to ADDR in one time programmable memory\n"
+	  "  -D    DATA            Write DATA to ADDR in one time programmable memory\n"
+	  "  -n                    Red LED on\n"
+	  "  -F                    Red LED off\n"
+     "  -g                    Green LED on\n"
+	  "  -G                    Green LED off\n"
+	  "  -i                    Display FPGA info\n"
+	  "  -o                    Display one time programmable data\n"
+     "  -m                    Display contents of non-volatile memory\n"
+     "  -M[xx:xx:xx:xx:xx:xx] Display [or optionally set] the MAC address\n"
+     "  -O                    Display odometer(hrs board has been running for)\n"
+     "  -B                    Display birthdate\n"
+	  "  -V                    Verbose output\n"
+	  "  -h                    This help screen\n",program_invocation_short_name);
+}
+
 
 int main(int argc, char **argv)
 {
    int c, devmem;
    unsigned int val_addr=0, val_data=0;
    unsigned int otp_addr, otp_data, secs = 0;
-   unsigned int display_otp=0, display_mem=0, display_mac=0;
+   int red_led_on=0, red_led_off=0, green_led_on=0, green_led_off=0;
+   unsigned int display_otp=0, display_mem=0, display_mac=0, set_mac=0 ;
 	unsigned int display_odom=0, did_something=0, display_bday=0;
-   unsigned int start_adc=0, raw=0, fpga_info=0;
-   unsigned int len, odom, bday;
+   unsigned int start_adc=0, raw=0, do_info=0;
+   unsigned int len; //, odom, bday;
    char str[80];
+   unsigned char new_mac[8];
    
 	if(argc == 1) {
 		usage(argv);
 		return 1;
 	}
 
-   if ((model=get_model())) { 
-      printf("model=%x\n", model);
+   if ((model=get_model())) {
       if (model != 0x7800) {
          fprintf(stderr, "Unsupported model\n");
          return 1;
       }
    }
    else {      
-      printf("model=unknown\n");
+      fprintf(stderr, "model=unknown\n");
       return 1;
    }
    twifd = silabs_init();
-	if(twifd == -1)
+	if(twifd == -1) {
+	  fprintf(stderr, "ERROR: Cannot initialize connection to Silabs via /dev/i2c-0\n");
 	  return 1;
+	}
 
    signal(SIGTERM, exit_gracefully);
 	signal(SIGHUP, exit_gracefully);
@@ -104,30 +143,30 @@ int main(int argc, char **argv)
 	signal(SIGPIPE, exit_gracefully);
 
 
-	while ((c = getopt(argc, argv, "s:fdr:S:A:D:inFVoOmMB")) != -1) {
+	while ((c = getopt(argc, argv, "s:fdr:S:A:D:inFgGVoOmM::B")) != -1) {
 	   switch(c) {
 			case 's': 
-			   printf("TBD: -%c option\n", c);
+
 			   secs = strtoul(optarg, NULL, 0);
-				if((secs > 0) && (secs <= (65535 * 8))){
-					if((secs % 8) > 0)  secs=(secs/8)+1;
-					else secs=(secs/8);
-				} else if(secs > (65535 * 8))
+				if((secs > 0) && (secs <= 524288)){
+				   did_something=1;
+				} else {
 					printf("Invalid sleep time,"
 					  "maximum sleep time is 524288\n");
-				did_something=1;
+				   secs = 0;
+				}
 			   break;
 
  			case 'f':
- 			   printf("TBD: -%c option\n", c);
+ 			   feed_wdt(8);
 			   break;
 
          case 'i':
- 			   fpga_info = 1;
+            do_info = 1;
 			   break;
 
          case 'd':
- 			   printf("TBD: -%c option\n", c);
+ 			   disable_wdt();
 			   break;
 
   			case 'r':
@@ -163,11 +202,19 @@ int main(int argc, char **argv)
 			   break;
 
  			case 'n':
- 			   printf("TBD: -%c option\n", c);
+ 			   red_led_on = 1;
 			   break;
 
  			case 'F':
- 			   printf("TBD: -%c option\n", c);
+ 			   red_led_off = 1;
+			   break;
+
+			case 'g':
+			   green_led_on = 1;
+			   break;
+
+ 			case 'G':
+ 			   green_led_off = 1;
 			   break;
 
 			case 'V':
@@ -175,6 +222,13 @@ int main(int argc, char **argv)
 				break;
 
 			case 'M':
+			   if (optarg) {
+			      if (! parseMacAddress(optarg, &new_mac[2])) {
+			         fprintf(stderr, "Invalid MAC: %s\n", optarg);
+			         return 1;
+			      }
+			      set_mac = 1;
+			   }
 				display_mac = 1;
 				did_something=1;
 				break;
@@ -198,112 +252,260 @@ int main(int argc, char **argv)
 			case 'm':
 				display_mem = 1;
 				did_something=1;
-				addr=70;
+				addr=1536;
 				break;
 
 			case 'h':	
 			default:
 				usage(argv);
 				return 1;
-	
 	   }	
    }
+
+   if (red_led_on && red_led_off) {
+      fprintf(stderr, "red LED on and off are mutually exclusive\n");
+   } else if (red_led_on || red_led_off) {
    
-   if (fpga_info) {
       devmem = open("/dev/mem", O_RDWR|O_SYNC);
 	   if (devmem < 0) {
 	      fprintf(stderr, "Error:  Can't open /dev/mem\n");
 	      return 1;
 	   }
 	   syscon = (unsigned int *) mmap(0, 4096,
-	     PROT_READ | PROT_WRITE, MAP_SHARED, devmem, 0xe8000000);
+	     PROT_READ | PROT_WRITE, MAP_SHARED, devmem, 0xFC081000);
+	   if (red_led_on)
+	      syscon[0x0c / 4] |= (1 << 20);
+	   else
+	      syscon[0x0c / 4] &= ~(1 << 20);
 
-	   printf("fpga_rev=0x%02X\n"
-	          "board_id=0x%04X\n", *syscon & 0xFF, (*syscon >> 8) & 0xFFFF);
+	   munmap((void*)syscon, 4096);
+
+	   close(devmem);
+   }
+
+   if (green_led_on && green_led_off) {
+      fprintf(stderr, "green LED on and off are mutually exclusive\n");
+   } else if (green_led_on || green_led_off) {
+
+      devmem = open("/dev/mem", O_RDWR|O_SYNC);
+	   if (devmem < 0) {
+	      fprintf(stderr, "Error:  Can't open /dev/mem\n");
+	      return 1;
+	   }
+	   syscon = (unsigned int *) mmap(0, 4096,
+	     PROT_READ | PROT_WRITE, MAP_SHARED, devmem, 0xFC081000);
+
+	   if (green_led_on)
+	      syscon[0x08 / 4] |= (1 << 30);
+	   else
+	      syscon[0x08 / 4] &= ~(1 << 30);
+	   munmap((void*)syscon, 4096);
+	   close(devmem);
+   }
+
+   if (do_info) {
+      unsigned char silabs_rev;
+      unsigned short adr;
+      unsigned int clk_straps, cpu_temp;
+      unsigned int pclk, nbclk, hclk, dclk, refclk, i;
+      unsigned char buf[14];
+      unsigned char mac[6];
+
+      printf("model=%x\n", model);
+
+      devmem = open("/dev/mem", O_RDWR|O_SYNC);
+	   if (devmem < 0) {
+	      fprintf(stderr, "Error:  Can't open /dev/mem\n");
+	      return 1;
+	   }
+	   syscon = (unsigned int *) mmap(0, 4096,
+	     PROT_READ | PROT_WRITE, MAP_SHARED, devmem, 0xFC081000);
+
+      silabs_read(2048, 1, &silabs_rev);
+
+      printf("fpga_rev=0x%02X\n"
+	          "silabs_rev=%d\n"
+	          "board_id=0x%04X\n",
+	          *syscon & 0xFF, silabs_rev, (*syscon >> 8) & 0xFFFF);
+
+      munmap((void*)syscon, 4096);
+      cpuregs =   (unsigned int *) mmap(0, 4096,
+	     PROT_READ | PROT_WRITE, MAP_SHARED, devmem, 0xf1018000);
+	   clk_straps = (cpuregs[0x600 / 4] >> 10) & 0x1F;
+	   switch(clk_straps) {
+	      case 0: pclk=666;nbclk=333; hclk=167; dclk=333; refclk=25;break;
+	      case 2: pclk=800;nbclk=400; hclk=200; dclk=400; refclk=25;break;
+	      case 4: pclk=1066;nbclk=533; hclk=267; dclk=533; refclk=25;break;
+	      case 6: pclk=1200;nbclk=600; hclk=300; dclk=600; refclk=25;break;
+	      case 8: pclk=1333;nbclk=667; hclk=333; dclk=667; refclk=25;break;
+	      case 0xc: pclk=1600;nbclk=800; hclk=400; dclk=800; refclk=25;break;
+	      case 0x10: pclk=1866;nbclk=933; hclk=467; dclk=933; refclk=25;break;
+	      default: pclk=0;nbclk=0; hclk=0; dclk=0; refclk=0;break;
+	   }
+
+	   munmap((void*)syscon, 4096);
+      cpuregs =   (unsigned int *) mmap(0, 4096,
+	     PROT_READ | PROT_WRITE, MAP_SHARED, devmem, 0xf10e4000);
+
+	   cpu_temp = ((4761 * (cpuregs[0x78 / 4] & 0x3ff))) / 10 - 279100; 
+
+	   printf("pclk=%d\n"
+	      "nbclk=%d\n"
+	      "hclk=%d\n"
+	      "dclk=%d\n"
+	      "refclk=%d\n"
+	      "cpu_temp=%d\n",
+	      pclk, nbclk, hclk, dclk, refclk, cpu_temp);
+
+		memset(buf, 0, sizeof(buf));
+		silabs_read(1280, sizeof(buf), buf);
+
+		for(i=0; i < 7; i++){
+		      unsigned short p = 0x3FF & *(unsigned short *)&buf[i*2];
+
+		      switch(i) {
+		         case 0: printf("cpu_core=%d\n", 2500 * p / 1024); break;
+		         case 1: printf("ram_1350=%d\n", 2500 * p / 1024); break;
+		         case 2: printf("v_1200=%d\n", 2500 * p / 1024); break;
+		         case 3: printf("v_1800=%d\n", 2500 * p / 1024); break;
+		         case 4: printf("v_8_30=%d\n", 2500 * p / 1024); break;
+		         case 5: printf("v_5va=%d\n", 5682 * p / 1024); break;
+		         case 6: printf("an_3300=%d\n", 5000 * p / 1024); break;
+		      }
+		   }
+
+	   adr = 0x006;   // 1536, byte-swapped
+		memset(mac, 0, sizeof(mac));
+
+		write(twifd, &adr , 2); //Write 2 byte reg address
+		read(twifd, mac, sizeof(mac));
+
+	   printf("hwaddr=%02x:%02x:%02x:%02x:%02x:%02x\n",
+	      mac[5],mac[4],mac[3],mac[2],mac[1],mac[0]);
 
    }
 
-
    if(start_adc) {
-      unsigned char data[32];		
-	       
-		volatile unsigned int *fpga, *sram;
+      unsigned char buf[24];
+
 		unsigned int i, loop=0;
 		int chans;
 		
 		chans = parsechans(str, len);
 		   if(chans < 0) return -1;
-				   
-		did_something = 1;
-   
-      while(! done) {		
-		   memset(data, 0, 32);
-		   if (read(twifd, data, 32)< 0)
-		      perror("read");
-		
-		   for(i=0; i < 32; i++)
-		      printf("%02X ", data[i]);
-		   printf("\n");   
-		
-		   sleep(1);
 
+		did_something = 1;
+
+      while(! done) {
+         char adrbuf[3];
+         short reg = 1280 + 14; // ADC regs are 1280-1535, 2 bytes each, LE
+
+		   memset(buf, 0, sizeof(buf));
+
+		   adrbuf[0] = ((reg >> 8) & 0xFF);
+		   adrbuf[1] = (reg & 0xFF);
+		   write(twifd, &adrbuf , 2); //Write 2 byte reg address
+		   i = read(twifd, buf, 10);
+
+		   for(i=0; i < 5; i++){
+		      unsigned short p;
+		      if (! (chans & (1 << i))) continue;
+
+		      p = 0x3FF & *(unsigned short *)&buf[(4-i)*2];
+
+		      if (raw)
+		         printf("[0x%08X, %d]=%d\n", loop++, i, p);
+		      else
+		         printf("[0x%08X, %d]=%d\n", loop++, i, 5000 * p / 1023);
+
+		   }
+
+
+		   /*
+		   for(i=0; i < 12; i++){
+		      unsigned short p = 0x3FF & *(unsigned short *)&buf[i*2];
+
+		      switch(i) {
+		         case 0: printf("cpu_core=%d\n", 2500 * p / 1024); break;
+		         case 1: printf("ram_1350=%d\n", 2500 * p / 1024); break;
+		         case 2: printf("v_1200=%d\n", 2500 * p / 1024); break;
+		         case 3: printf("v_1800=%d\n", 2500 * p / 1024); break;
+		         case 4: printf("v_8_30=%d\n", 2500 * p / 1024); break;
+		         case 5: printf("v_5va=%d\n", 5682 * p / 1024); break;
+		         case 6: printf("an_3300=%d\n", 5000 * p / 1024); break;
+		         case 7: printf("adc_4=%d\n", 5000 * p / 1024); break;
+		         case 8: printf("adc_3=%d\n", 5000 * p / 1024); break;
+		         case 9: printf("adc_2=%d\n", 5000 * p / 1024); break;
+		         case 10: printf("adc_1=%d\n", 5000 * p / 1024); break;
+		         case 11: printf("adc_0=%d\n", 5000 * p / 1024); break;
+		      }
+		   }
+
+			*/
+
+		   usleep(100000);
       }
    }
-        
-	if(secs > 0) {
-	   printf("TBD: implement sleep function\n");
-	}
-	
+
+
 	if(val_data && val_addr) {
 		printf("TBD: implement OTP write function\n");
 	}	
-		
+
 	if(display_odom) {
       printf("TBD: implement odometer function\n");
 	}
-		
+
 	if(display_bday) {
         printf("TBD: implement birthdate function\n");
 	}
-	
+
+	if (set_mac) {
+	   new_mac[0] = 6;
+	   new_mac[1] = 0;
+	   write(twifd, &new_mac , sizeof(new_mac));
+	}
+
 	if(display_mac) {
-	    printf("TBD: implement MAC function\n");
+	   unsigned char mac[6];
+	   unsigned short adr = 0x006;   // 1536, byte-swapped
+
+		memset(mac, 0, sizeof(mac));
+
+		write(twifd, &adr , 2); //Write 2 byte reg address
+		read(twifd, mac, sizeof(mac));  
+
+	   printf("HWaddr %02x:%02x:%02x:%02x:%02x:%02x\n", 
+	      mac[5],mac[4],mac[3],mac[2],mac[1],mac[0]);
 	}
 
 	if(display_mem || display_otp) {
-	      printf("TBD: implement display memory function\n");
+	   unsigned char buf[16];
+	   char adrbuf[2];
+	   int i;
+
+	   memset(buf, 0, sizeof(buf));
+
+		adrbuf[0] = ((addr >> 8) & 0xFF);
+		adrbuf[1] = (addr & 0xFF);
+
+	   write(twifd, &adrbuf , 2); //Write 2 byte reg address
+		read(twifd, buf, sizeof(buf));  
+
+		for(i=0; i < 4; i++)
+		   fwrite(&buf[i * sizeof(unsigned int)], sizeof(unsigned int), 1, stdout);
+	   fflush(stdout);
+
+	}
+
+	if (did_something && secs > 0) {
+	   do_silabs_sleep(secs * 100);
 	}
 
    return 0;
 }
 
 
-static void usage(char **argv) 
-{
-   fprintf(stderr, "Usage: %s [OPTION] ...\n"
-	  "Modify state of TS-7800 hardware.\n"
-	  "\n"
-	  "General options:\n"
-	  "  -s    seconds         Number of seconds to sleep for\n"
-	  "  -f                    Feed the WDT for 8s\n"
-	  "  -d                    Disable the WDT\n"
-	  "  -r    CHANS           Sample ADC channels CHANS, e.g. \"0-2,4\", \"1,3,4\"" 
-                                 "output raw data to standard out\n"
-     "  -S    CHANS           Sample ADC channels CHANS, e.g. \"0-2,4\", \"1,3,4\"" 
-                                  "output string parseable data to standard out\n"
-	  "  -A    ADDR            Write DATA to ADDR in one time programmable memory\n"
-	  "  -D    DATA            Write DATA to ADDR in one time programmable memory\n"
-	  "  -n                    Red LED on\n"
-	  "  -F                    Red LED off\n"
-	  "  -i                    Display FPGA info\n"
-	  "  -o                    Display one time programmable data\n"
-     "  -m                    Display contents of non-volatile memory\n"
-     "  -M                    Display MAC address\n"
-     "  -O                    Display odometer(hrs board has been running for)\n"
-     "  -B                    Display birthdate\n"     
-	  "  -V                    Verbose output\n"			
-	  "  -h                    This help screen\n",program_invocation_short_name);
-}
 
 static int parsechans(const char *str, unsigned int len) 
 {
@@ -379,7 +581,7 @@ static int get_model(void)
 	    return 0;
 	}
 	fread(mdl, 256, 1, proc);
-	if (strstr(mdl, "TS-7800v2"))
+	if (strcasestr(mdl, "TS-7800v2") || strcasestr(mdl, "TS-7800-v2"))
 	   return 0x7800;
 	   
 	else {
@@ -393,11 +595,97 @@ static int silabs_init(void)
 	static int fd = -1;
 	fd = open("/dev/i2c-0", O_RDWR);
 	if(fd != -1) {
-		if (ioctl(fd, I2C_SLAVE_FORCE, 0x4a) < 0) {
-			perror("Microcontroller did not ACK 0x4a\n");
+		if (ioctl(fd, I2C_SLAVE_FORCE, SILABS_CHIP_ADDRESS) < 0) {
+			perror("SiLabs did not ACK\n");
 			return -1;
 		}
 	}
 
 	return fd;
+}
+
+static int silabs_read(unsigned short reg, unsigned short cnt, unsigned char *buf)
+{
+   char adrbuf[3];
+
+   adrbuf[0] = ((reg >> 8) & 0xFF);
+   adrbuf[1] = (reg & 0xFF);
+   write(twifd, &adrbuf , 2); //Write 2 byte reg address
+   return read(twifd, buf, cnt);
+}
+
+/** must be in the form XX:XX:XX:XX:XX:XX, where XX is a hex number */
+
+static int parseMacAddress(const char *str, unsigned char *buf)
+{
+   int n;
+   unsigned int addr[6];
+
+   if (sscanf(str, "%x:%x:%x:%x:%x:%x", 
+      &addr[5],&addr[4],&addr[3],
+      &addr[2],&addr[1],&addr[0]) != 6)
+      return 0;
+
+   for(n=0; n < 6; n++) {
+      if (addr[n] > 255) return 0;
+      buf[n] = addr[n];
+   }
+
+   return 1;
+
+}
+
+static inline void feed_wdt(int t)
+{
+   unsigned char data[] = {
+      0x04, 0x00, // 1024, byte-swapped
+      0x20, 0x03, 0, 0  // 800
+   };
+
+	write(twifd, data, sizeof(data));
+	enable_wdt();
+}
+
+static inline void enable_wdt(void)
+{
+   unsigned char data[] = {
+      0x04, 0x04, // 1028, byte-swapped
+      1
+   };
+
+	write(twifd, data, sizeof(data)); 
+}
+
+static inline void disable_wdt(void)
+{
+   unsigned char data[] = {
+      0x04, 0x04, // 1028, byte-swapped
+      0
+   };
+
+	write(twifd, data, sizeof(data)); 
+}
+
+static inline void do_silabs_sleep(unsigned int deciseconds)
+{
+   unsigned char data[6];
+
+   printf("Sleeping for %d deciseconds...\n", deciseconds);
+
+   disable_wdt();
+
+   data[0] = 0x04; // 1024, byte-swapped
+   data[1] = 0;
+   data[2] = deciseconds & 0xFF;
+   data[3] = (deciseconds >> 8) & 0xFF;
+   data[4] = (deciseconds >> 16) & 0xFF;
+   data[5] = (deciseconds >> 24) & 0xFF;
+
+   write(twifd, data, 6);
+
+   data[0] = 0x04; // 1028, byte-swapped
+   data[1] = 0x04;
+   data[2] = 2;
+
+   write(twifd, data, 3);
 }
