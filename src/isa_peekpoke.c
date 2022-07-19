312a3ffdbad8253d24b1ae7677e8644e1e4da6de
Michael Peters <mpeters@embeddedTS.com>
2022-07-19 22:57:38 +0000

    Fixed bugs.

--- src/isa_peekpoke.c
+++ src/isa_peekpoke.c
@@ -11,8 +11,8 @@
 
 void usage(char *name)
 {
-	fprintf(stderr, "Usage %s <i/m> <bit width> <address> [value]\n", name);
-	fprintf(stderr, "\tEg: %s i 8 0x0\n", name);
+	fprintf(stderr, "Usage %s <io/mem> <bit width> <address> [value]\n", name);
+	fprintf(stderr, "\tEg: %s i 8 0x140\n", name);
 }
 
 int main(int argc, char **argv) {
@@ -36,13 +36,13 @@ int main(int argc, char **argv) {
  	/* syscon_peek */
 	if (argc == 4) {
 		if ((bus == 'm' || bus == 'M' ) && sz == 8)
-			val = isa_mem_peek8(syscon, off);
+			val = isa_mem_peek8(isa, off);
 		else if ((bus == 'm' || bus == 'M' ) && sz == 16)
-			val = isa_mem_peek16(syscon, off);
+			val = isa_mem_peek16(isa, off);
 		else if ((bus == 'i' || bus == 'I' ) && sz == 8)
-			val = isa_io_peek8(syscon, off);
+			val = isa_io_peek8(isa, off);
 		else if ((bus == 'i' || bus == 'I' ) && sz == 16)
-			val = isa_io_peek16(syscon, off);
+			val = isa_io_peek16(isa, off);
 		else {
 			usage(argv[0]);
 			return 1;
@@ -50,13 +50,13 @@ int main(int argc, char **argv) {
 		printf("0x%llX\n", val);
 	} else { /* syscon_poke */
 		if (sz == 1)
-			isa_mem_poke8(syscon, off, val);
+			isa_mem_poke8(isa, off, val);
 		else if (sz == 2)
-			isa_mem_poke16(syscon, off, val);
+			isa_mem_poke16(isa, off, val);
 		else if (sz == 3)
-			isa_io_poke8(syscon, off, val);
+			isa_io_poke8(isa, off, val);
 		else if (sz == 4)
-			isa_io_poke16(syscon, off, val);
+			isa_io_poke16(isa, off, val);
 		else {
 			usage(argv[0]);
 			return 1;
